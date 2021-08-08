e35707371e783b174b07e361c9057ac31c51e815
Steve <asapcrafter@gmail.com>
2021-08-08 13:20:46 -0700

    added more divs

---
+++ dist/main.js
@@ -0,0 +1,8 @@
+console.log("Testing webpack");
+
+content =  document.getElementById('content');
+
+var headline = document.createElement("div");
+    headline.setAttribute("id", "headline");
+    content.appendChild(headline);
+
